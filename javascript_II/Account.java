5855da1a6cd7eeb051cb16a0510426c37cde2bd7
henrysalkever <henry.salkever@icloud.com>
2025-03-01 11:21:38 +0000

    more classwork

---
+++ javascript_II/Account.java
@@ -0,0 +1,11 @@
+public class Account {
+    
+    private int var1;
+    private int var2;
+
+    public void setData(int new_num){
+        this.var1 = new_num;
+        this.var2 = new_num;
+    }
+
+}
---
+++ javascript_II/AccountTest.java
@@ -0,0 +1,10 @@
+public class AccountTest {
+    
+    public static void main(String[] args){
+        Account obj2 = new Account();
+        Account obj3 = new Account();
+
+        obj2.setData(3);
+        obj3.setData(4);
+    }
+}
